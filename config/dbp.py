2a3a87e2cbbc40be451dc247a094f4407ff4186e
davidrqch8800 <141936170+davidrqch8800@users.noreply.github.com>
2024-01-22 22:19:37 -0500

    Update dbp.py

--- config/dbp.py
+++ config/dbp.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://root:contra@localhost:3306/database'
+URL_DATABASE = 'mysql+pymysql://root:contra@localhost:3306/xdaea'
 
 engine = create_engine(URL_DATABASE)
 
